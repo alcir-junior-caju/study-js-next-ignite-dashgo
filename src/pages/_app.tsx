14f1df3e88f1fa6c547a4c2f1fb760e7cd350c9b
Alcir Junior <junior@cajucomunica.com.br>
2021-05-24 13:27:21 -0300

    feat: Add ChackraUI and Settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { theme } from "../styles/theme";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
