abc6fc7ae9b6672b0ed0e2b620fdbc0ce7c41f64
Alcir Junior <junior@cajucomunica.com.br>
2021-05-24 14:31:35 -0300

    feat: Add SignIn ChakraUI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
-const Home = () => {
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import Input from '../components/Form/Input';
+
+const SignIn = () => {
   return (
-    <>
-      <h1>DashGo</h1>
-    </>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex as="form" flexDir="column" w="100%" maxW={360} bg="gray.800" p={8} borderRadius={8}>
+        <Stack spacing={4}>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt={6} colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   );
 }
 
-export default Home;
+export default SignIn;
