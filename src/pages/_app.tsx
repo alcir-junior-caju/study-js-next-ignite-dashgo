664caac09b4b7a3de3914fe013064785359bac81
Alcir Junior <junior@cajucomunica.com.br>
2021-05-26 10:55:29 -0300

    feat: Add Mirage to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { SidebarDrawerProvider } from "../context/SidebarDrawerContext";
+import { makeServer } from "../services/mirage";
 import { theme } from "../styles/theme";
+
+if (process.env.NODE_ENV === 'development') makeServer();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
